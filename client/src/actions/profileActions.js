ae732c9b3bd824270446a0c068e9a6ee789657f2
Smallulu <junhaol1206@gmail.com>
2018-11-16 13:01:55 -0800

    finished adding experience and education functions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -53,6 +53,32 @@ export const clearCurrentProfile = () => {
    }
 }
 
+// add experience 
+export const addExperience = (expData, history) => dispatch => {
+   axios
+      .post('/api/profile/experience', expData)
+      .then(res => history.push('/dashboard'))
+      .catch(err => 
+         dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+         })   
+      );
+}
+
+// add education 
+export const addEducation = (eduData, history) => dispatch => {
+   axios
+      .post('/api/profile/education', eduData)
+      .then(res => history.push('/dashboard'))
+      .catch(err => 
+         dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+         })   
+      );
+}
+
 // delete account 
 export const deleteAccount = () => dispatch => {
    if(window.confirm('Are you sure to delete account? It can not be undone!')) {
