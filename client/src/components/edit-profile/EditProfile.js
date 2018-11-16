8752dba0bd8f664ff85ebff42e38a54acaf7f169
Smallulu <junhaol1206@gmail.com>
2018-11-16 13:53:56 -0800

    finish experience and education credential deletions

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -8,6 +8,7 @@ import SelectListGroup from '../common/SelectListGroup';
 import { createProfile, getCurrentProfile } from '../../actions/profileActions';
 import { withRouter } from 'react-router-dom';
 import isEmpty from '../../validation/is-empty';
+import { Link } from 'react-router-dom';
 
 class EditProfile extends Component {
 
@@ -175,6 +176,7 @@ class EditProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">Go Back</Link>
               <h1 className="display-4 text-center">Edit Your Profile</h1>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
