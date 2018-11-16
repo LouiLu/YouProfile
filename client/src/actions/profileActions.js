3c458391780e8dc9f3763af46054e456cb9cf331
Smallulu <junhaol1206@gmail.com>
2018-11-16 15:38:53 -0800

    create profiles page for public social propose

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,8 +4,29 @@ import {
          PROFILE_LOADING, 
          CLEAR_CURRENT_PROFILE, 
          GET_ERRORS, 
-         SET_CURRENT_USER 
+         SET_CURRENT_USER, 
+         GET_PROFILES
       } from './types';
+
+// get all user profiles
+export const getProfiles = () => dispatch => {
+   dispatch(setProfileLoading());
+
+   // REQUEST
+   axios
+      .get('/api/profile/all')
+      .then(res =>
+         dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+         })
+      )
+      .catch(err => dispatch({
+            type: GET_PROFILE,
+            payload: null
+         })
+      );
+}
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
