d278cfd2a4c2c01a5a4742fd7dd34e5ba812a28d
Smallulu <junhaol1206@gmail.com>
2018-11-15 17:06:27 -0800

    create the create profile route

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,217 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
+
+class CreateProfile extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubusername: '',
+      bio: '',
+      twitter: '',
+      facebook: '',
+      linkedin: '',
+      youtube: '',
+      instagram: '',
+      errors: {}
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    //console.log("submit");
+  }
+
+  render() {
+    const { errors, displaySocialInputs } = this.state;
+
+    let socialInputs;
+
+    if (displaySocialInputs) {
+      socialInputs = (
+        <div>
+          <InputGroup
+            placeholder="Twitter Profile URL"
+            name="twitter"
+            icon="fab fa-twitter"
+            value={this.state.twitter}
+            onChange={this.onChange}
+            error={errors.twitter}
+          />
+          <InputGroup
+            placeholder="Facebook Profile URL"
+            name="facebook"
+            icon="fab fa-facebook"
+            value={this.state.facebook}
+            onChange={this.onChange}
+            error={errors.facebook}
+          />
+          <InputGroup
+            placeholder="LinkedIn Profile URL"
+            name="linkedin"
+            icon="fab fa-linkedin"
+            value={this.state.linkedin}
+            onChange={this.onChange}
+            error={errors.linkedin}
+          />
+          <InputGroup
+            placeholder="YouTube Profile URL"
+            name="youtube"
+            icon="fab fa-youtube"
+            value={this.state.youtube}
+            onChange={this.onChange}
+            error={errors.youtube}
+          />
+          <InputGroup
+            placeholder="Instagram Profile URL"
+            name="instagram"
+            icon="fab fa-instagram"
+            value={this.state.instagram}
+            onChange={this.onChange}
+            error={errors.instagram}
+          />
+        </div>
+      )
+    }
+
+    // default select options
+    const options = [
+      {label: '* Select Your Current Status', value: 0},
+      {label: 'Developer', value: 'Developer'},
+      {label: 'Junior Developer', value: 'Junior Developer'},
+      {label: 'Senior Developer', value: 'Senior Developer'},
+      {label: 'Intership', value: 'Intership'},
+      {label: 'Co-op', value: 'Co-op'},
+      {label: 'Student', value: 'Student'},
+      {label: 'Project Manager', value: 'Project Manager'},
+      {label: 'Professor', value: 'Professor'},
+      {label: 'Others', value: 'Others'}
+    ];
+
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Please fill out some information for youelf
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for you profile URL."
+                />
+                <SelectListGroup 
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  error={errors.status}
+                  options={options}
+                  info="Your current status."
+                />
+                <TextFieldGroup
+                  placeholder="Company"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                  info="Your current employer."
+                />
+                <TextFieldGroup
+                  placeholder="Website"
+                  name="website"
+                  value={this.state.website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info="Your personal website."
+                />
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info="Your current employer."
+                />
+                <TextFieldGroup
+                  placeholder="* Skills"
+                  name="skills"
+                  value={this.state.skills}
+                  onChange={this.onChange}
+                  error={errors.skills}
+                  info="Let us know your skills. Pleae use comma to seperate the skills. (eg. HTML, JavaScript, CSS, etc.)"
+                />
+                <TextFieldGroup
+                  placeholder="Github Username"
+                  name="githubusername"
+                  value={this.state.githubusername}
+                  onChange={this.onChange}
+                  error={errors.githubusername}
+                  info="Your Github links for your latest repos, including your username."
+                />
+                <TextAreaFieldGroup
+                  placeholder="A short Bio of yourself"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info="Tell a little bit about yourself."
+                />
+
+                <div className="mb-3">
+                  <button 
+                    className="btn btn-dark"
+                    onClick={ () => {
+                      this.setState( (prevState) => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }))
+                    }} 
+                    >
+                  Add Social Network Links (Otional)
+                  </button>
+                </div>
+                {socialInputs}
+                <input type="submit" value="submit" className="btn btn-info btn-block mt-4"/>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import { Link } from 'react-router-dom';
 
 class Dashboard extends Component {
    componentDidMount() {
@@ -18,7 +19,21 @@ class Dashboard extends Component {
     if (profile === null || loading) {
       dashboardContent = <Spinner />;
     } else {
-      dashboardContent = <h1>Hello no profile</h1>
+      // check if profile data is empty or not
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>Display profile</h4>
+      } else {
+        // user with empty profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not created a profile yet.</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+            Create Profile
+            </Link>
+          </div>
+        );
+      }
     }
 
     return (
