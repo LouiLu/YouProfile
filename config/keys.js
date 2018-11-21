c499e5ec4df345def1a60efb41f83b1de14ee5c6
Smallulu <junhaol1206@gmail.com>
2018-11-21 14:42:09 -0800

    add DB security with ENV checking

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds117701.mlab.com:17701/dev-smallulu",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+   mongoURI: process.env.MONGOURI,
+   secretOrKey: process.env.SECRET_OR_KEY
+ };
+ 
